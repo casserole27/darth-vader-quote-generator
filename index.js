e83d7ec5a174d5e2f6f0e59cfa2813939e3873b5
C Lewis <casserole.lewis@gmail.com>
2023-02-25 11:40:20 -0500

    Initial project file setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const quotes = [
+    {
+        quote: "Today is your opportunity to build the tomorrow you want.",
+        author: "Ken Poirot"
+    },
+    {
+        quote: "When you have a dream, you've got to grab it and never let go.",
+        author: "Carol Burnett"
+    },
+    {
+        quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+        author: "Winston Churchill"
+    },
+    {
+        quote: "You define your own life. Don't let other people write your script.",
+        author: "Oprah Winfrey"
+    },
+    {
+        quote: "Someone once told me to never dream. I said NEVER SAY NEVER",
+        author: "Justin Bieber"
+    }
+]
